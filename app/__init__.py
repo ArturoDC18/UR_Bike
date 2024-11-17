89096214ed951d22a9abb8f54b32ddb6e26af8cb
Arturo <vadc1809@gmail.com>
2024-11-16 20:45:43 -0500

    Added functional loging system

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:rbUWeKiW44O4o1egPHwd@recs-db.cn62c4wqs8na.us-east-2.rds.amazonaws.com:3306/recs-db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, forms
--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 # Bike Part Model
 class BikePart(db.Model):
@@ -20,11 +23,22 @@ class Place(db.Model):
 
     def __repr__(self):
         return '<Place {}>'.format(self.name)
-class User(db.Model):
+        
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    admin = db.Column(db.Boolean, default=False)
     password_hash = db.Column(db.String(128))
+    admin=db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def set_admin(self):
+        self.admin=True
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(id):
+        return db.session.get(User, int(id))
